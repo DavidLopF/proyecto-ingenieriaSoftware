39f3f361606881c5a5a3f333a71c62b2ddfb980c
Esteban <lecardenasc@unbosque.edu.co>
2022-06-05 01:02:22 -0500

    administrador funciones

--- src/controllers/admin-controller.js
+++ src/controllers/admin-controller.js
@@ -4,7 +4,64 @@ const bcrypt = require('bcrypt');
 
 class AdminController {
 
-    getTems(req, res) { }
+    getTems(req, res) { 
+       //get all the competitors from the database with the table team in data base
+        db.Team.findAll()
+        .then(async function(teams) {
+            const competitors = await db.Competitor.findAll();
+            const captain = await db.Captain.findAll();
+            const user = await db.User.findAll();
+            console.log('pasa por aqui');
+            //create an array of objects with the data of the teams
+            const teamsData = {
+                team : {
+                    team_id : '',
+                    team_name : '',
+                    captain_user_id : '',
+                    captain_name : '',
+                    competitor_user_id_1 : '',
+                    competitor_name_1 : '',
+                    competitor_user_id_2 : '',
+                    competitor_name_2 : '',
+                }
+                };
+                console.log('pasa por aqui');
+                for(let tema of teams) {
+                    console.log('entra');
+                    let data = [] ;
+                    let TEAM = tema.dataValues
+                    data.push(TEAM);
+                    for(let cap of captain) {
+                       
+                        if(tema.id == cap.team_id) {
+                            let CAPTAIN = cap.dataValues;
+                           data.push(CAPTAIN);
+                        }
+                    }
+                    for(let comp of competitors) {
+                        
+                        if(tema.id == comp.team_id) {
+                            let COMPETITOR = comp.dataValues;
+                           data.push(COMPETITOR);
+                        }
+                    }
+                    console.log('aqui---->',data);
+                }
+
+            //return the array of objects with the data of the teams
+            res.status(200).json({
+                message: 'Teams',
+                teams,
+                competitors,
+                captain,
+                user,
+            });
+        })
+
+        .catch(function(err) {
+            res.send(err);
+        });
+    }
 
     getCSV(req, res) { }
 
--- src/routes/admin-route.js
+++ src/routes/admin-route.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 const router = Router()
-const ViewController = require('../controllers/admin-controller')
-const viewController = new ViewController();
+const Admin = require('../controllers/admin-controller')
+const admin = new Admin();
 
-
+router.get('/teams', (req, res) => {
+    admin.getTems(req, res);
+})
 
 
 module.exports = router
